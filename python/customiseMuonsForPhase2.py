e6f1ef1c7b385ceb9a065484ea2f6784a178cd88
Carlo Battilana <carlo.battilana@gmail.com>
2017-02-12 19:39:04 +0100

    Change for Gaelle, run tracking

--- python/customiseMuonsForPhase2.py
+++ python/customiseMuonsForPhase2.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 def customiseMuons(process):
 
-    process = customiseL1Unoacking(process)
+    process = customiseL1Unpacking(process)
     process = customiseL1Seeds(process)
     process = removeHLTIsoMu24Steps(process)
     process = addTrigReport(process)
@@ -25,7 +25,7 @@ def customiseL1Unpacking(process):
 def customiseL1Seeds(process):
    
     if hasattr(process,"HLT_IsoMu24_v4") :
-        print "[customiseL1Seeds] Add cms.Ignore() to IsoMu24 l1 related filters"
+        print "[customiseL1Seeds] Add cms.Ignore() to IsoMu24 L1 related filters"
 
         process.HLT_IsoMu24_v4.replace(process.hltL1sSingleMu22,cms.ignore(process.hltL1sSingleMu22))
         process.HLT_IsoMu24_v4.replace(process.hltL1fL1sMu22L1Filtered0,cms.ignore(process.hltL1fL1sMu22L1Filtered0))
@@ -37,7 +37,7 @@ def removeHLTIsoMu24Steps(process):
     if hasattr(process,"HLT_IsoMu24_v4") :
         print "[removeHLTIsoMu24Steps] Removing steps not yet migrated to Phase2"
     
-        process.HLT_IsoMu24_v4.remove(process.HLTL3muonrecoSequence)
+        #process.HLT_IsoMu24_v4.remove(process.HLTL3muonrecoSequence)
         process.HLT_IsoMu24_v4.remove(process.hltL3fL1sMu22L1f0L2f10QL3Filtered24Q)
         process.HLT_IsoMu24_v4.remove(process.HLTMu24IsolationSequence)
         process.HLT_IsoMu24_v4.remove(process.hltL3crIsoL1sMu22L1f0L2f10QL3f24QL3trkIsoFiltered0p09) 
