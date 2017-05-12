f0a7fd86370938906541c295f6624f274dec12fe
Carlo Battilana <carlo.battilana@gmail.com>
2017-05-12 15:56:55 +0200

    Run the full paths

--- python/customiseMuonsForPhase2.py
+++ python/customiseMuonsForPhase2.py
@@ -39,18 +39,21 @@ def useRpcSimDigis(process):
 
     return process
 
+
+
+
+# Keeping them here if debugging is needed
+# not really used now
+
 def removeHLTIsoMu24Steps(process):
 
     if hasattr(process,"HLT_IsoMu24_v4") :
         print "[removeHLTIsoMu24Steps] Removing steps not yet migrated to Phase2"
     
-        process.HLT_IsoMu24_v4.replace(process.HLTL2muonrecoSequence, process.HLTL2muonrecoSequence \
-                                                                      + process.HLTDoLocalPixelSequence \
-                                                                      + process.HLTDoLocalStripSequence)
-        process.HLT_IsoMu24_v4.remove(process.HLTL3muonrecoSequence)
-        process.HLT_IsoMu24_v4.remove(process.hltL3fL1sMu22L1f0L2f10QL3Filtered24Q)
-        process.HLT_IsoMu24_v4.remove(process.HLTMu24IsolationSequence)
-        process.HLT_IsoMu24_v4.remove(process.hltL3crIsoL1sMu22L1f0L2f10QL3f24QL3trkIsoFiltered0p09) 
+        #process.HLT_IsoMu24_v4.remove(process.HLTL3muonrecoSequence)
+        #process.HLT_IsoMu24_v4.remove(process.hltL3fL1sMu22L1f0L2f10QL3Filtered24Q)
+        #process.HLT_IsoMu24_v4.remove(process.HLTMu24IsolationSequence)
+        #process.HLT_IsoMu24_v4.remove(process.hltL3crIsoL1sMu22L1f0L2f10QL3f24QL3trkIsoFiltered0p09) 
 
     return process
 
@@ -59,11 +62,8 @@ def removeHLTMu50Steps(process):
     if hasattr(process,"HLT_Mu50_v5") :
         print "[removeHLTMu50Steps] Removing steps not yet migrated to Phase2"
     
-        process.HLT_Mu50_v5.replace(process.HLTL2muonrecoSequence, process.HLTL2muonrecoSequence \
-                                                                      + process.HLTDoLocalPixelSequence \
-                                                                      + process.HLTDoLocalStripSequence)
         #process.HLT_Mu50_v5.remove(process.HLTL3muonrecoSequence)
-        process.HLT_Mu50_v5.remove(process.hltL3fL1sMu22Or25L1f0L2f10QL3Filtered50Q)
+        #process.HLT_Mu50_v5.remove(process.hltL3fL1sMu22Or25L1f0L2f10QL3Filtered50Q)
 
     return process
 
@@ -72,26 +72,8 @@ def removeHLTIsoTkMu24Steps(process):
     if hasattr(process,"HLT_IsoTkMu24_v4") :
         print "[removeHLTIsoTkMu24Steps] Removing steps not yet migrated to Phase2"
     
-#        process.HLT_IsoTkMu24_v4.replace(process.HLTHighPt24TrackerMuonSequence, process.HLTDoLocalPixelSequence \
-#                                                                                 + process.HLTDoLocalStripSequence \
-#                                                                                 + process.hltL1MuonsPt15 \
-#                                                                                 + process.hltPixelLayerTriplets \
-#                                                                                 + process.hltIter0HighPtTkMuPixelTracksFilter \
-#                                                                                 + process.hltIter0HighPtTkMuPixelTracksFitter \
-#                                                                                 + process.hltIter0HighPtTkMuPixelTracksTrackingRegions \
-#                                                                                 + process.hltIter0HighPtTkMuPixelTracksHitDoublets \
-#                                                                                 + process.hltIter0HighPtTkMuPixelTracksHitTriplets \
-#
-#                                                                                 + process.hltIter0HighPtTkMuPixelTracks \
-#                                                                                 + process.hltIter0HighPtTkMuPixelSeedsFromPixelTracks \
-#                                                                                 + process.hltIter0HighPtTkMuCkfTrackCandidates \
-#                                                                                 + process.hltIter0HighPtTkMuCtfWithMaterialTracks \
-#                                                                                 #+ process.hltIter0HighPtTkMuTrackSelectionHighPurity \
-#                                                                                 # process.HLTIterativeTrackingHighPtTkMuIteration0
-#                                                                                 )
-#        process.HLT_IsoTkMu24_v4.remove(process.hltL3fL1sMu22f0TkFiltered24Q)
-        process.HLT_IsoTkMu24_v4.remove(process.HLTTkMu24IsolationSequence)
-        process.HLT_IsoTkMu24_v4.remove(process.hltL3fL1sMu22L1f0Tkf24QL3trkIsoFiltered0p09) 
+        #process.HLT_IsoTkMu24_v4.remove(process.HLTTkMu24IsolationSequence)
+        #process.HLT_IsoTkMu24_v4.remove(process.hltL3fL1sMu22L1f0Tkf24QL3trkIsoFiltered0p09) 
 
     return process
 
