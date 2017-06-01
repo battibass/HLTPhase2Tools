1a92567c0db6eedc52848f833cce2c848813ce67
Carlo Battilana <carlo.battilana@gmail.com>
2017-06-01 11:09:05 +0200

    Change FEVTDEBUG

--- python/customiseForPhase2.py
+++ python/customiseForPhase2.py
@@ -82,6 +82,7 @@ def customiseEventContent(process):
     if hasattr(process,"FEVTDEBUGHLToutput") :
         print "[customiseEventContent] Customise event content to keep hltIter* "
         process.FEVTDEBUGHLToutput.outputCommands.append('keep *_hltIter*_*_HLT')
+        process.FEVTDEBUGHLToutput.outputCommands.append('keep *_MuonsIter*_*_HLT')
         process.FEVTDEBUGHLToutput.outputCommands.append('keep Phase2TrackerCluster1D*_*_*_HLT')
         
     return process
