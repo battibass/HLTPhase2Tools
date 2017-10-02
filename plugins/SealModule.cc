4358419623616dbb33f90b58cae1eaa292a5c341
Carlo Battilana <carlo.battilana@gmail.com>
2017-10-02 08:08:19 +0200

    Add HLTFilterToTrackProducer

---
+++ plugins/SealModule.cc
@@ -0,0 +1,5 @@
+#include "HLTrigger/Phase2/plugins/HLTFilterToTrackProducer.h"
+#include "FWCore/PluginManager/interface/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+DEFINE_FWK_MODULE(HLTFilterToTrackProducer);
